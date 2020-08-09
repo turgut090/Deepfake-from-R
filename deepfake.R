8093342a449a50ab38401eafa6d74e59a548d1a1
henry090 <turqut.a.314@gmail.com>
2020-08-09 10:44:54 +0400

    end

--- deepfake.R
+++ deepfake.R
@@ -76,24 +76,25 @@ Sys.time() - t
 
 train_ = train_ %>% mutate(dirr = gsub(video_id,replacement = '',pattern = '\\.mp4'))
 
+nms_dirs = dir('train_deep_fake')
+train_ = train_ %>% filter(dirr %in% nms_dirs)
+
 fakes <- train_ %>% filter(labels=='FAKE')
 reals <- train_ %>% filter(labels=='REAL')
 
-#list_of_dirs <- list.dirs('~/Downloads/train_deep_fake',full.names = FALSE)
-
-
 fakes_dir <- paste('train_deep_fake/',fakes$dirr,sep='')
-reals_dir <- paste('train_deep_real/',reals$dirr,sep='')
+reals_dir <- paste('train_deep_fake/',reals$dirr,sep='')
 
-data_prepare_faces <- function(dirs,new_dir_name) {
+data_prepare_faces <- function(dirs, new_dir_name) {
   
   dirs = dirs
   dir.create(new_dir_name)
-  for(i in 1:dirs){
+  for(i in 1:length(dirs)){
     dir_idx = dirs[i]
     for (j in 1:length(list.files(dir_idx))) {
       filess = list.files(dir_idx)
-      unconf <- ocv_read(paste(dir_idx,'/',filess[j],sep = ''))
+      path_img = paste(dir_idx,'/',filess[j],sep = '')
+      unconf <- ocv_read(path_img)
       faces <- ocv_face(unconf)
       facemask <- ocv_facemask(unconf)
       df = attr(facemask, 'faces')
@@ -101,40 +102,92 @@ data_prepare_faces <- function(dirs,new_dir_name) {
       rectY = (df$y - df$radius)
       x = (df$x + df$radius) 
       y = (df$y + df$radius)
-      
-      edited = image_crop(imh, "49x49+66+34")
-      edited = image_crop(imh, paste(x-rectX+1,'x',x-rectX+1,'+',rectX, '+',rectY,sep = ''))
-      edited
-      image_write(edited,'/home/turgut/Downloads/video/frame_1_face.png')
+      if(!nrow(df)==0){
+        for (z in 1:nrow(df)) {
+          imh  = image_read(path_img)
+          edited = image_crop(imh, paste(x-rectX[z]+1,'x',x-rectX[z]+1,'+',rectX[z], '+',rectY[z],sep = ''))
+          edited
+          image_write(edited,paste(new_dir_name,'/','fake_',sample(1:10000,1),sample(1:10000,1),z,sep = ''))
+        }
+      }
     }
+    print(paste('Done',i,'out of',length(dirs)))
   }
 }
 
+data_prepare_faces(fakes_dir,'fakes')
+data_prepare_faces(reals_dir,'reals')
+
+library(ff)
+dir.create('fakes_reals')
+dir.create('fakes_reals/reals')
+dir.create('fakes_reals/fakes')
+
+from_r <- "~/Downloads/reals"           
+to_r   <- "~/Downloads/fakes_reals/reals"
+from_f <- "~/Downloads/fakes"           
+to_f   <- "~/Downloads/fakes_reals/fakes"
+file.move(from_r,to_r)
+file.move(from_f,to_f)
+
+files_to_png = list.files('~/Downloads/fakes_reals',recursive = T,full.names = T)
+sapply(1:length(files_to_png), function(x) file.rename(files_to_png[x], paste(files_to_png[x],'.png',sep = '')))
+
+library(keras)
+library(tensorflow)
+
+tensorflow::tf_version()
+physical_devices = tf$config$list_physical_devices('GPU')
+tf$config$experimental$set_memory_growth(physical_devices[[1]],T)
+
+train_dir = '~/Downloads/fakes_reals'
+width = 150L
+height = 150L
+epochs = 10
+
+train_datagen = image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest",
+  validation_split=0.2
+)
 
 
-unconf <- ocv_read('/home/turgut/Downloads/video/frame_1.png')
-faces <- ocv_face(unconf)
-faces
-#ocv_write(faces, 'faces.jpg')
-facemask <- ocv_facemask(unconf)
-df = attr(facemask, 'faces')
-rectX = (df$x - df$radius) 
-rectY = (df$y - df$radius)
-x = (df$x + df$radius) 
-y = (df$y + df$radius)
-#crop_img = self.img[y:(y+2*r), x:(x+2*r)]
+train_generator <- flow_images_from_directory(
+  train_dir,                  
+  train_datagen,             
+  target_size = c(width,height), 
+  batch_size = 10,
+  class_mode = "binary"
+)
 
-imh  = image_draw(image_read('/home/turgut/Downloads/video/frame_1.png'))
-rect(rectX, rectY, x, y, border = "red", lty = "dashed", lwd = 2)
-dev.off()
+# Build the model ---------------------------------------------------------
 
-image_info(imh)->info
-x-rectX+1
-y-rectY+1
-df
+conv_base <- application_vgg16(
+  weights = "imagenet",
+  include_top = FALSE,
+  input_shape = c(width, height, 3)
+)
 
-edited = image_crop(imh, "49x49+66+34")
-edited = image_crop(imh, paste(x-rectX+1,'x',x-rectX+1,'+',rectX, '+',rectY,sep = ''))
-edited
-image_write(edited,'/home/turgut/Downloads/video/frame_1_face.png')
+model <- keras_model_sequential() %>% 
+  conv_base %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 256, activation = "relu") %>% 
+  layer_dense(units = 1, activation = "sigmoid")
 
+model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 2e-5),
+  metrics = c("accuracy")
+)
+
+history <- model %>% fit_generator(
+  train_generator,
+  steps_per_epoch = ceiling(train_generator$samples/train_generator$batch_size),
+  epochs = 10
+)
